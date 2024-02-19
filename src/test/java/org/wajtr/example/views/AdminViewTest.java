ab35638c71e485b1dd1a50b25fa5efd1a634ab05
Martin Vysny <mavi@vaadin.com>
2024-02-19 10:04:27 +0200

    Initial test batch

---
+++ src/test/java/org/wajtr/example/views/AdminViewTest.java
@@ -0,0 +1,33 @@
+package org.wajtr.example.views;
+
+import com.vaadin.flow.component.UI;
+import org.junit.jupiter.api.Test;
+import org.wajtr.example.views.helloworld.HelloWorldView;
+import org.wajtr.example.views.login.LoginView;
+
+import static com.github.mvysny.kaributesting.v10.LocatorJ._assertOne;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AdminViewTest extends AbstractAppTest {
+    @Test
+    public void inaccessibleByAnonymous() {
+        UI.getCurrent().navigate(AdminView.class);
+        _assertOne(LoginView.class);
+    }
+
+    @Test
+    public void inaccessibleByUser() {
+        loginUser();
+        assertThrows(RuntimeException.class, () -> {
+            UI.getCurrent().navigate(AdminView.class);
+        });
+        _assertOne(LoginView.class);
+    }
+
+    @Test
+    public void accessibleByAdmin() {
+        loginAdmin();
+        UI.getCurrent().navigate(AdminView.class);
+        _assertOne(AdminView.class);
+    }
+}
---
+++ src/test/java/org/wajtr/example/views/helloworld/HelloWorldViewTest.java
@@ -0,0 +1,31 @@
+package org.wajtr.example.views.helloworld;
+
+import com.vaadin.flow.component.UI;
+import org.junit.jupiter.api.Test;
+import org.wajtr.example.views.AbstractAppTest;
+import org.wajtr.example.views.about.AboutView;
+import org.wajtr.example.views.login.LoginView;
+
+import static com.github.mvysny.kaributesting.v10.LocatorJ._assertOne;
+
+public class HelloWorldViewTest extends AbstractAppTest {
+    @Test
+    public void inaccessibleByAnonymous() {
+        UI.getCurrent().navigate(HelloWorldView.class);
+        _assertOne(LoginView.class);
+    }
+
+    @Test
+    public void accessibleByUser() {
+        loginUser();
+        UI.getCurrent().navigate(HelloWorldView.class);
+        _assertOne(HelloWorldView.class);
+    }
+
+    @Test
+    public void accessibleByAdmin() {
+        loginAdmin();
+        UI.getCurrent().navigate(HelloWorldView.class);
+        _assertOne(HelloWorldView.class);
+    }
+}
